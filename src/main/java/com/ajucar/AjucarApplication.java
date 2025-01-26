0da157634896bf63ac6a558d4e0cbd2d8040d265
Douglas <contato.cunhadoug@gmail.com>
2025-01-26 17:44:15 -0300

    init project

---
+++ src/main/java/com/ajucar/AjucarApplication.java
@@ -0,0 +1,13 @@
+package com.ajucar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjucarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjucarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajucar/AjucarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajucar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjucarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
